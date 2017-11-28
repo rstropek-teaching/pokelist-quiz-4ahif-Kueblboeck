362fa9d9c8998190e2ad21e08d4031043d735b08
Kueblboeck <31965050+Kueblboeck@users.noreply.github.com>
2017-11-28 16:34:28 +0100

    Optimised code as far as possible

--- script.ts
+++ script.ts
@@ -1,63 +1,75 @@
-(async function () {
-    const pokelist = await $.get('https://pokeapi.co/api/v2/pokemon/');
+let offset = 0;
+const pokeUrl = "https://pokeapi.co/api/v2/pokemon/";
+const appendUrl = "?limit=20&offset=";
 
-    let html = '';
-    for (const pokemon of pokelist.results) {
-        html += `<li>${pokemon.name}</li><button onclick=detailsPage('${pokemon.url}')>Details</button>`
-    }
+(async function initialize() {
+    const pokelist = await $.get(pokeUrl);
 
+    let html = getPokeApi(pokelist);
+
+    offset = 20;
     $('#pokemons')[0].innerHTML = html;
 })();
 
-var offset = 20;
+function getPokeApi(pokelist: any) {
+    let html = '';
+    for (const pokemon of pokelist.results) {
+        html += `<h4 class="has-text-black">${pokemon.name}</h4><button class="button is-danger" onclick=listDetails('${pokemon.url}')>Details</button>`
+    }
+    return html;
+}
 
-function nextPage() {
-    (async function () {
-        const pokelist = await $.get('https://pokeapi.co/api/v2/pokemon/?limit=20&offset=' + offset);
+async function navigate(navigation: number) {
+    if (navigation == 1) {
+        const pokelist = await $.get(pokeUrl + appendUrl + offset);
 
-        let html = '';
-        for (const pokemon of pokelist.results) {
-            html += `<li>${pokemon.name}</li><button onclick=detailsPage('${pokemon.url}')>Details</button>`
-        }
+        const html = getPokeApi(pokelist);
 
         $('#pokemons')[0].innerHTML = html;
         offset += 20;
-    })();
-}
-
-function previousPage() {
-    if (offset > 0) {
-        offset -= 20;
-    } else {
-
-    }
-
-    (async function () {
-        const pokelist = await $.get('https://pokeapi.co/api/v2/pokemon/?limit=20&offset=' + offset);
-
-        let html = '';
-        for (const pokemon of pokelist.results) {
-            html += `<li>${pokemon.name}</li><button onclick=detailsPage('${pokemon.url}')>Details</button>`
+    } else if (navigation == 2) {
+        if (offset > 0) {
+            offset -= 20;
         }
+        const pokelist = await $.get(pokeUrl + appendUrl + offset);
+
+        const html = getPokeApi(pokelist);
 
         $('#pokemons')[0].innerHTML = html;
-    })();
+    }
+
 }
 
-function detailsPage(url){
-    var newWindow = window.open('');
-    newWindow.document.body.innerHTML = "<html><div id=details>Test</div></html>";
-    (async function () {
-        const pokelist = await $.get(url);
-
-        let html = '';
-        for (const pokemon of pokelist.results) {
-            html += `<li>Name: ${pokemon.name}</li>`
-            html += `<li>Weight: ${pokemon.weight}</li>`
-            html += `<li>Abilities: ${pokemon.abilitites}</li>`
-            html += `<div>lorem ipsum</div>`
+async function listDetails(url: string) {
+    let modal = document.getElementById("myModal");
+    modal.style.display = "block";
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
         }
+    }
+    let pokeName = document.getElementById("pokeName");
+    let pokeWeight = document.getElementById("pokeWeight");
+    let pokeImage = document.getElementById("pokeImage");
+    let pokeAbilities = document.getElementById("pokeAbilities");
 
-        newWindow.document.body.innerHTML = html;
-    })();
+    const response = await fetch(url);
+    const pokemon = await response.json();
+
+    pokeName.innerHTML = pokemon.name;
+
+    pokeWeight.innerHTML = "Weight: " + pokemon.weight;
+
+    pokeImage.innerHTML = "";
+    let image = document.createElement("img");
+    image.setAttribute("id", "image");
+    image.setAttribute("src", pokemon.sprites.front_default);
+    pokeImage.appendChild(image);
+
+    pokeAbilities.innerHTML = "";
+    let abilities: string = "<h2> Abilities </h2><br/>";
+    for (const ability of pokemon.abilities) {
+        abilities += ability.ability.name + "<br/>";
+    }
+    pokeAbilities.innerHTML = abilities;
 }
